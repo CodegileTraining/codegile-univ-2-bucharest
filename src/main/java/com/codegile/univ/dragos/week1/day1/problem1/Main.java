64ee05421f5a5e7002fbfd69f347f0f4ff0188da
Dragos Heroiu <dragos.heroiu95@gmail.com>
2018-02-12 17:06:50 +0200

    Problem 1 solved ( count how many numbers are int and how many float from an input file) .

--- src/main/java/com/codegile/univ/dragos/week1/day1/problem1/Main.java
+++ src/main/java/com/codegile/univ/dragos/week1/day1/problem1/Main.java
@@ -1,8 +1,67 @@
 package com.codegile.univ.dragos.week1.day1.problem1;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int floats = 0;
+        int integers = 0;
+        String currentLine;
+
+        try (
+                InputStream is = ClassLoader.getSystemResourceAsStream("numbers.txt");
+                BufferedReader br = new BufferedReader(new InputStreamReader(is))
+        ) {
+            while ((currentLine = br.readLine()) != null) {
+                String[] lineStrings = currentLine.split(" ");
+                for (String currentValue : lineStrings) {
+                    if (isInt(currentValue)) {
+                        integers++;
+                    }
+                    if (isFloat(currentValue)) {
+                        floats++;
+                    }
+                }
+            }
+
+            System.out.println("Integers length :" + integers);
+            System.out.println("Floats length :" + floats);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isInt(String s) {
+
+        try {
+
+            Integer.parseInt(s);
+            return true;
+
+        } catch (NumberFormatException e) {
+
+            return false;
+        }
+    }
+
+    private static boolean isFloat(String s) {
+
+        try {
+
+            Float.parseFloat(s);
+            return true;
+
+        } catch (NumberFormatException e) {
+
+            return false;
+
+        } catch (NullPointerException e) {
+
+            return false;
+        }
     }
 }
