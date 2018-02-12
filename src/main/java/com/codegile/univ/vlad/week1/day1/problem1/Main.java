42a27b9de1da474f140ebad8d6140ab1cee4d364
Vlad Fusea <vladfusea04@gmail.com>
2018-02-12 18:12:21 +0200

    Edit after TODO

--- src/main/java/com/codegile/univ/vlad/week1/day1/problem1/Main.java
+++ src/main/java/com/codegile/univ/vlad/week1/day1/problem1/Main.java
@@ -6,54 +6,49 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        int intNumbersLength = 0;
-        int realNumbersLength = 0;
+        int intNumbersCount = 0;
+        int realNumbersCount = 0;
 
-        InputStream is = ClassLoader.getSystemResourceAsStream("numbers.txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String line = null; // TODO: Remove unused initializer
-        try{
-            while((line = br.readLine()) !=  null){
+
+        String line;
+        try (InputStream is = ClassLoader.getSystemResourceAsStream("numbers.txt");
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
                 String[] array = line.split(" ");
-                // TODO: Replace with foreach loop
-                for(int i=0; i<= array.length-1; i++){
-                    if(checkIfStringIsInteger(array[i])) {
-                        intNumbersLength++; // TODO: Count
+
+                for (String i : array) {
+                    if (isInteger(i)) {
+                        intNumbersCount++;
                     }
-                    if(checkIfStringIsReal(array[i])){
-                        realNumbersLength++;
+                    if (isReal(i)) {
+                        realNumbersCount++;
                     }
                 }
             }
-        }catch (IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
-            // TODO: You have to close BufferedReaders, InputStreams etc. with the .close() method - very important
-            // TODO: Find out how to use try-with-resources to avoid writing a finally block
         }
-        System.out.println("Integer numbers: "+intNumbersLength); // TODO: Always use CTRL+ALT+L to format your code before pushing
-        System.out.println("Real numbers: "+realNumbersLength);
 
 
+        System.out.println("Integer numbers: " + intNumbersCount);
+        System.out.println("Real numbers: " + realNumbersCount);
     }
 
-    public static boolean checkIfStringIsInteger(String string){ // TODO: Use clearer naming for these methods.
-        try{
+    private static boolean isInteger(String string) {
+        try {
             Integer.parseInt(string);
-        }catch(NumberFormatException e){
-            return false;
-        }catch(NullPointerException e){ // TODO: Investigate this warning
+        } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
         return true;
     }
 
-    public static boolean checkIfStringIsReal(String string){
-        try{
+    private static boolean isReal(String string) {
+        try {
             Float.parseFloat(string);
-        }catch(NumberFormatException e){
-            return false;
-        }catch(NullPointerException e){
+        } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
         return true;
