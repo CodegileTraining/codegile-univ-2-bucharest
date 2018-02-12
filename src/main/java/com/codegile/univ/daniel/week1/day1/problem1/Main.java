12b9062ca1b141789f71b7b85e6f21ef6624a35f
Daniel Haiducu <haivukodani18@gmail.com>
2018-02-12 17:18:39 +0200

    TO DO list done

--- src/main/java/com/codegile/univ/daniel/week1/day1/problem1/Main.java
+++ src/main/java/com/codegile/univ/daniel/week1/day1/problem1/Main.java
@@ -4,35 +4,54 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException { // TODO: Remove unused throws statement
-        int nrOfInt=0; // TODO: Always use CTRL+ALT+L to format your code
-        int nrOfReal=0;
-        BufferedReader br = null; // TODO: Remove unused initializer.
-            InputStream is = ClassLoader.getSystemResourceAsStream("file.txt");
-            br = new BufferedReader(new InputStreamReader(is));
-            try {
-                String x; // TODO: Name your variables in a more explicit way, so that it is easier to read the code
-                while ((x = br.readLine()) != null) {
-                    String[] y = x.split(" ");
-                    for (String s: y)
-                    {
-                        try{
-                        int i = Integer.parseInt(s); // TODO: No need to save the value in a variable - we only care about the exception being thrown
-                        nrOfInt++;
-                        } catch ( NumberFormatException e) {
-                            double d = Double.parseDouble(s); // TODO: Same thing, no need to save value
-                            nrOfReal++; // TODO: What happens if the value in s is not an integer or a real number?
+    public static void main(String[] args) {
+        int nrOfInt = 0;
+        int nrOfReal = 0;
+        InputStream is = ClassLoader.getSystemResourceAsStream("file.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        try {
+            String contentOfFile;
+            while ((contentOfFile = br.readLine()) != null) {
+                String[] contentOfFileAsStringArray = contentOfFile.split(" ");
+                for (String s : contentOfFileAsStringArray) {
+                    if ( isNumeric(s)==true ) {
+                        try {
+                            Integer.parseInt(s);
+                            nrOfInt++;
+                        } catch (NumberFormatException e) {
+                            Float.parseFloat(s);
+                            nrOfReal++;
                         }
                     }
-                    System.out.println("\n");
-                    System.out.println("File content: " + x);
-                    System.out.println("\n");
-                    System.out.println("Number of integers: " +  nrOfInt);
-                    System.out.println("Number of doubles: " + nrOfReal); // TODO: Are you sure you counted all the real numbers??
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                int allRealNumbers = nrOfReal + nrOfInt;
+                System.out.println("\n");
+                System.out.println("File content: " + contentOfFile);
+                System.out.println("\n");
+                System.out.println("Number of integers: " + nrOfInt);
+                System.out.println("Number of doubles: " + allRealNumbers);
             }
-            // TODO: Always close InputStreams and BufferedReaders etc! Use the .close() methods in a finally { } block
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+                is.close();
+            } catch (IOException ignored) {
+            }  // I've done it like this because without try catch it trows an error:
+            // 'unhandled exception java.io.exception'
+        }
+    }
+    public static boolean isNumeric(String str)
+    {
+        try
+        {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
     }
 }
