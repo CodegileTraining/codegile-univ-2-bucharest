4b3394ed2bf0fa66726eb93ceab13a48f94d1951
Daniel Haiducu <haivukodani18@gmail.com>
2018-02-12 17:37:04 +0200

    final solution

--- src/main/java/com/codegile/univ/daniel/week1/day1/problem1/Main.java
+++ src/main/java/com/codegile/univ/daniel/week1/day1/problem1/Main.java
@@ -7,43 +7,35 @@ public class Main {
     public static void main(String[] args) {
         int nrOfInt = 0;
         int nrOfReal = 0;
-        InputStream is = ClassLoader.getSystemResourceAsStream("file.txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        try {
+        try (
+                InputStream is = ClassLoader.getSystemResourceAsStream("file.txt");
+                BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+
             String contentOfFile;
             while ((contentOfFile = br.readLine()) != null) {
                 String[] contentOfFileAsStringArray = contentOfFile.split(" ");
                 for (String s : contentOfFileAsStringArray) {
-                    if (isNumeric(s) == true) {
+                    if (isNumeric(s)) {
+                        nrOfReal++;
                         try {
                             Integer.parseInt(s);
                             nrOfInt++;
-                        } catch (NumberFormatException e) {
-                            Float.parseFloat(s);
-                            nrOfReal++;
+                        } catch (NumberFormatException ignored) {
                         }
                     }
                 }
-                int allRealNumbers = nrOfReal + nrOfInt;
                 System.out.println("\n");
                 System.out.println("File content: " + contentOfFile);
                 System.out.println("\n");
                 System.out.println("Number of integers: " + nrOfInt);
-                System.out.println("Number of doubles: " + allRealNumbers);
+                System.out.println("Number of doubles: " + nrOfReal);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-                is.close();
-            } catch (IOException ignored) {
-            }  // I've done it like this because without try catch it trows an error:
-            // 'unhandled exception java.io.exception'
         }
     }
 
-    public static boolean isNumeric(String str) {
+    private static boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
         } catch (NumberFormatException nfe) {
