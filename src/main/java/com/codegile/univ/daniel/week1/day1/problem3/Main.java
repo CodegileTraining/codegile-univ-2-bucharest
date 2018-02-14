f6e400f0e96a505376eb4d5426bc6cb412c331ae
Paul Hristea <paul.hristea@codegile.com>
2018-02-14 19:14:33 +0200

    Added some todos.

--- src/main/java/com/codegile/univ/daniel/week1/day1/problem3/Main.java
+++ src/main/java/com/codegile/univ/daniel/week1/day1/problem3/Main.java
@@ -11,23 +11,23 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> contentOfFileAsStringList;
-        int nrOfLetters;
+        int nrOfLetters; // TODO: Try to declare and initialize your variables at the same time (don't declare all variables at start, only where you need them)
         FileReader fr = new FileReader();
 
         contentOfFileAsStringList = fr.stringListOfFile("file.txt");
         Collections.sort(contentOfFileAsStringList);
 
-        System.out.printf("\n");
+        System.out.printf("\n"); // TODO: Use System.out.println("..."); - this will automatically print a "\n" character after your text
         System.out.printf("This is the sorted content: " + contentOfFileAsStringList);
 
-        for (int i = 0; i < contentOfFileAsStringList.size(); i++) {
+        for (int i = 0; i < contentOfFileAsStringList.size(); i++) { // TODO: Try with foreach loop
             String word = contentOfFileAsStringList.get(i).toLowerCase();
             if ("aeiou".indexOf(word.charAt(0)) != -1) {
                 nrOfLetters = word.length();
                 String stringNrOfLetters = String.valueOf(nrOfLetters);
                 contentOfFileAsStringList.set(i, stringNrOfLetters);
             } else {
-                continue;
+                continue; // TODO: Read the warning - do we need this statement?
             }
         }
         System.out.printf("\n");
