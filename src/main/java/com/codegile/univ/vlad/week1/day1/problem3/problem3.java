04286332d659f52a97bea1307efaf11e855ddb18
Vlad Fusea <vladfusea04@gmail.com>
2018-02-15 17:57:58 +0200

    citise si scriere fisier html valid + verificare cuvinte

--- src/main/java/com/codegile/univ/vlad/week1/day1/problem3/problem3.java
+++ src/main/java/com/codegile/univ/vlad/week1/day1/problem3/problem3.java
@@ -1,13 +1,14 @@
 package com.codegile.univ.vlad.week1.day1.problem3;
 
 // TODO: Always remove useless imports
-import javax.imageio.IIOException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 
 public class problem3 {
@@ -16,33 +17,33 @@ public class problem3 {
         try (InputStream is = ClassLoader.getSystemResourceAsStream("stringuri.txt");
              BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             String line;
-            int numarLitere = 0; // TODO: Use English for variable names (letterCount)
+            int letterCount = 0; // TODO: Use English for variable names (letterCount)
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
                 String[] array = line.split(" ");
-                ArrayList lista = new ArrayList(); // TODO: Declare as interface and use generics (List<String> list = new ArrayList<>())
-                for (String word : array) { // TODO: Investigate warning on the 'for'
-                    lista.add(word);
-                }
-                Collections.sort(lista);
-                System.out.println(lista);
+                List<String> list = new ArrayList<>(); // TODO: Declare as interface and use generics (List<String> list = new ArrayList<>())
+                Collections.addAll(list, array);
+                Collections.sort(list);
+                System.out.println(list);
                 for (String word : array) {
                     String firstLetter = word.substring(0, 1);
                     if ("aeiouAEIOU".contains(firstLetter)) {
-                        for (int i = 0; i < word.length(); i++) { // TODO: Why are you manually counting the letters?
-                            numarLitere++;
-                        }
-                        System.out.println(word); // TODO: You should print all the words, but replace the ones starting with a vowel (and don't print the word in that case)
-                        System.out.println(numarLitere);
+                        letterCount = word.length();
+                        // TODO: You should print all the words, but replace the ones starting with a vowel (and don't print the word in that case)
+                        System.out.println(letterCount);
+                    } else {
+                        System.out.println(word);
                     }
-
-                    numarLitere = 0;
-
                 }
 
+                letterCount = 0;
+
             }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
--- src/main/java/com/codegile/univ/vlad/week1/day1/problem4/problem4.java
+++ src/main/java/com/codegile/univ/vlad/week1/day1/problem4/problem4.java
@@ -1,40 +1,62 @@
 package com.codegile.univ.vlad.week1.day1.problem4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class problem4 {
     public static void main(String[] args) {
         try (InputStream is = ClassLoader.getSystemResourceAsStream("htmlvalid.txt");
              BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             String line;
-            int cuvintea2bCount = 0; // TODO: Find a better name for this variable
-            int bCount = 0;
+            int wordCount = 0; // TODO: Find a better name for this variable
+            List<String> lines = new ArrayList<>();
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                line = line.replaceAll("<p>", "<span>")
-                        .replaceAll("</p", "</span>"); // TODO: You have to replace the tags in the file (write to the file)
-                String[] array = line.split(" ");
-                for (String word : array) {
-                    for (int i = 0; i <= word.length() - 1; i++) {
-                        if (word.charAt(i) == 'a' || word.charAt(i) == 'b') { // TODO: Try to find a solution using RegEx
-                            if (word.charAt(i) == 'b') {
-                                bCount++;
-                            }
-                        } else break;
-                    }
-                    if (bCount % 2 == 0 && bCount >1) { // TODO: Why must there be at least 1 letter B?
-                        cuvintea2bCount++;
-                        System.out.println(word);
-                    }
-                    bCount=0;
-                }
-                System.out.println("Numarul de cuvinte cautate este: "+cuvintea2bCount);
+                lines.add(line);
             }
+            replace(lines, "<p>", "<span>");
+            replace(lines, "</p>", "</span>");
+            writeToFile("src/main/resources/htmlvalid.txt", lines);
+            for (String linie : lines) {
+                String[] array = linie.split(" +");
+                for (String word : array) {
+                    if (word.matches("((a*ba*ba*)+|a+)")) {
+                        wordCount++;
+                    }
+                }
+            }
+            System.out.println("Numarul de cuvinte cautate este: " + wordCount);
+
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile(String filePath, List<String> lines) {
+        File fileToBeModified = new File(filePath);
+        try (FileWriter writer = new FileWriter(fileToBeModified)) {
+            for (String line : lines) {
+                writer.append(line)
+                        .append("\n");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void replace(List<String> list, String oldString, String newString) {
+
+        for (int i = 0; i < list.size(); i++) {
+            String replacedString = list.get(i);
+            replacedString = replacedString.replaceAll(oldString, newString);
+            list.set(i, replacedString);
         }
     }
 }
