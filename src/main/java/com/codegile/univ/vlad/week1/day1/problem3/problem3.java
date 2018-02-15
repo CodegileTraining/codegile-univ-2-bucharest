7b2829f3795919704d40cba6a1eb0f2c3bd0cf6e
Paul Hristea <paul.hristea@codegile.com>
2018-02-15 13:25:47 +0200

    Added some todos.

--- src/main/java/com/codegile/univ/vlad/week1/day1/problem3/problem3.java
+++ src/main/java/com/codegile/univ/vlad/week1/day1/problem3/problem3.java
@@ -1,5 +1,6 @@
 package com.codegile.univ.vlad.week1.day1.problem3;
 
+// TODO: Always remove useless imports
 import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,12 +16,12 @@ public class problem3 {
         try (InputStream is = ClassLoader.getSystemResourceAsStream("stringuri.txt");
              BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             String line;
-            int numarLitere = 0;
+            int numarLitere = 0; // TODO: Use English for variable names (letterCount)
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
                 String[] array = line.split(" ");
-                ArrayList lista = new ArrayList();
-                for (String word : array) {
+                ArrayList lista = new ArrayList(); // TODO: Declare as interface and use generics (List<String> list = new ArrayList<>())
+                for (String word : array) { // TODO: Investigate warning on the 'for'
                     lista.add(word);
                 }
                 Collections.sort(lista);
@@ -28,10 +29,10 @@ public class problem3 {
                 for (String word : array) {
                     String firstLetter = word.substring(0, 1);
                     if ("aeiouAEIOU".contains(firstLetter)) {
-                        for (int i = 0; i < word.length(); i++) {
+                        for (int i = 0; i < word.length(); i++) { // TODO: Why are you manually counting the letters?
                             numarLitere++;
                         }
-                        System.out.println(word);
+                        System.out.println(word); // TODO: You should print all the words, but replace the ones starting with a vowel (and don't print the word in that case)
                         System.out.println(numarLitere);
                     }
 
