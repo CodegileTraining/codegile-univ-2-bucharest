11bbed1d46e43b63ef10bf6415eb244e7931ae7b
raluca <ralupopa@yahoo.com>
2018-02-19 18:52:05 +0200

    file reader done

---
+++ src/main/java/com/codegile/univ/raluca/week1/day1/problem1/Main.java
@@ -0,0 +1,20 @@
+package com.codegile.univ.raluca.week1.day1.problem1;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (InputStream is = ClassLoader.getSystemResourceAsStream("text.txt");
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+
+            while ((line= br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
