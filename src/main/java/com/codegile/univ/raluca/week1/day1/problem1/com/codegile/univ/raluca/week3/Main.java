b305046703d93d803adef19aa719ec6921bb1506
raluca <ralupopa@yahoo.com>
2018-03-01 14:24:20 +0200

    customList unfinished

---
+++ src/main/java/com/codegile/univ/raluca/week1/day1/problem1/com/codegile/univ/raluca/week3/Main.java
@@ -0,0 +1,16 @@
+package com.codegile.univ.raluca.week1.day1.problem1.com.codegile.univ.raluca.week3;
+
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> list = new CustomList<>();
+        list.add(" 23");
+        list.add("326");
+        System.out.println(list);
+
+
+    }
+}
