81b2076acd688525fd1a0879583d0ede4660b65c
raluca <ralupopa@yahoo.com>
2018-03-06 15:47:52 +0200

    customMap

---
+++ src/main/java/com/codegile/univ/com/codegile/univ/raluca/week4/Main.java
@@ -0,0 +1,9 @@
+package com.codegile.univ.com.codegile.univ.raluca.week4;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<> maps = new CustomMap();
+    }
+}
